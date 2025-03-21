0e33a0c826455413ccc8091090aa3e55689002fe
Deependra-Das <iamdeep75@gmail.com>
2025-03-21 16:05:42 +0530

    Added IService & RegisterService Changes

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using System;
+using System.Collections.Generic;
 using Roguelike.Utilities;
 
 namespace Roguelike.Main
 {
     public class GameService : GenericMonoSingleton<GameService>
     {
+        private Dictionary<Type, IService> _services = new Dictionary<Type, IService>();
+
         protected override void Awake()
         {
             base.Awake();
@@ -12,9 +16,59 @@ namespace Roguelike.Main
 
         private void Start()
         {
-            Debug.Log("GameService Running");
+            RegisterServices();
+            InjectDependencies();
+            Debug.Log("Game Service Running");
         }
 
-    }
+        private void RegisterServices()
+        {
 
+        }
+
+        public void InjectDependencies()
+        {
+
+        }
+
+        public void RegisterService<T>(IService service) where T : IService
+        {
+            Type serviceType = typeof(T);
+            if (!_services.ContainsKey(serviceType))
+            {
+                _services[serviceType] = service;
+            }
+            else
+            {
+                Debug.LogWarning($"{serviceType} is already registered.");
+            }
+        }
+
+        public void InitializeService<T>(IService service, params object[] dependencies) where T : IService
+        {
+            Type serviceType = typeof(T);
+            if (!_services.ContainsKey(serviceType))
+            {
+                Debug.LogWarning($"{serviceType} is already registered.");
+            }
+            else
+            {
+                service.Initialize(dependencies);
+            }
+        }
+
+        public T GetService<T>() where T : IService
+        {
+            Type serviceType = typeof(T);
+            if (_services.ContainsKey(serviceType))
+            {
+                return (T)_services[serviceType];
+            }
+            else
+            {
+                Debug.LogError($"{serviceType} is not registered.");
+                return default;
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/Utilities/IService.cs
@@ -0,0 +1,7 @@
+namespace Roguelike.Utilities
+{
+    public interface IService
+    {
+        void Initialize(params object[] dependencies);
+    }
+}
