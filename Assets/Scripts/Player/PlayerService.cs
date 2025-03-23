20de965785674a1524871420c33ea93088409deb
Deependra-Das <iamdeep75@gmail.com>
2025-03-23 12:31:32 +0530

    Added Player Service Script

---
+++ Assets/Scripts/Player/PlayerService.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using Roguelike.Main;
+
+namespace Roguelike.Player
+{
+    public class PlayerService
+    {
+        private PlayerScriptableObject playerScriptableObject;
+        private PlayerController playerController;
+
+        public PlayerService(PlayerScriptableObject playerScriptableObject)
+        {
+            this.playerScriptableObject = playerScriptableObject;
+            SubscribeToEvents();
+        }
+
+        private void SubscribeToEvents() => GameService.Instance.EventService.OnLevelSelected.AddListener(SpawnPlayer);
+
+        private void UnsubscribeToEvents() => GameService.Instance.EventService.OnLevelSelected.RemoveListener(SpawnPlayer);
+
+        public void SpawnPlayer(int levelId)
+        {
+            playerController = new PlayerController(playerScriptableObject);
+            UnsubscribeToEvents();
+        }
+
+        public PlayerController GetPlayer() => playerController;
+    }
+}
